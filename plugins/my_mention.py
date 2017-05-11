ad65d7b2d3bc6b09ff4614c232a4afae0c567b26
yudaishimanaka <gurutaminn.h8@gmail.com>
2017-05-11 16:47:09 +0900

    add file

---
+++ plugins/my_mention.py
@@ -0,0 +1,27 @@
+from slackbot.bot import respond_to, listen_to, default_reply
+
+
+@respond_to('task')
+@listen_to('task')
+def reply_task_list(message):
+    message.reply("タスクリストを表示しますね！！！")
+
+
+@respond_to('愛こそすべて')
+@listen_to('愛こそすべて')
+def reply_guiltykiss(message):
+    message.reply("guilty kiss!!")
+
+
+@respond_to('可愛い')
+@listen_to('可愛い')
+@respond_to('かわいい')
+@listen_to('かわいい')
+def reply_kawaii(message):
+    message.reply("ありがとう♡")
+    message.react('heart')
+
+
+@default_reply()
+def default_reply(message):
+    message.reply("私、わかるよ！！！")
---
+++ slackbot_settings.sample.py
@@ -0,0 +1,5 @@
+API_TOKEN = "取得したAPI_TOKEN"  # https://my.slack.com/services/new/bot
+
+DEFAULT_REPLY = "default_message"  # デフォルトのメッセージ
+
+PLUGINS = ['plugins']  # Botの機能を設置するディレクトリ
