d7805975c387689d3c5be7534651779b6c06f3e8
yudaishimanaka <gurutaminn.h8@gmail.com>
2017-05-12 00:48:06 +0900

    edit usage.py

---
+++ plugins/usage.py
@@ -0,0 +1,18 @@
+from slackbot.bot import respond_to, listen_to
+
+
+@respond_to('usage')
+@listen_to('usage')
+def usage(message):
+    msg = "使用法だよっ!!\n" \
+          "*コマンド一覧*\n" \
+          ">タスクを開く:*open [task_title]/[overview]/[task_level]/[date]*\n" \
+          ">タスクを閉じる:*close [task_title]*\n" \
+          "*オプションの説明*\n" \
+          ">[task_title] - タスクタイトルを指定\n" \
+          ">[overview] - タスク内容を指定\n" \
+          ">[task_level] - low(低い):normal(普通):high(高い)の中から指定\n" \
+          ">[date] - mm:dd:yy-mm:dd:yy\n" \
+          "*コマンド例* (タスクを開く)\n" \
+          ">open test/これはテストタスクです/high/04:01:2017-04:10:2017"
+    message.reply(msg)
