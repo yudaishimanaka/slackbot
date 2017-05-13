a33351279bf5f0024362a147fef2a84b873bcfcc
yudaishimanaka <gurutaminn.h8@gmail.com>
2017-05-14 03:54:48 +0900

    combine function

--- plugins/todo.py
+++ plugins/todo.py
@@ -11,6 +11,20 @@ connection = pymysql.connect(
 )
 
 
+@respond_to(r'^task\s*$')
+@listen_to(r'^task\s*$')
+def task_list(message):
+    with connection.cursor() as cursor:
+        msg = "タスク一覧だよっ!!\n"
+        sql = "select * from Task where username='yudai' and status='on'"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for value in results:
+            msg += (">*" + str(value[1]) + "* (" + str(value[4]) + "まで)\n")
+
+        message.reply(str(msg))
+
+
 @respond_to(r'^open\s([亜-熙ぁ-んァ-ヶa-zA-Z._?\/^\S].*?)\/([亜-熙ぁ-んァ-ヶa-zA-Z._?\/^\S].*?)'
             r'\/(low|norml|high)\/(\d{2}:\d{2}:\d{4}-\d{2}:\d{2}:\d{4})$')
 @listen_to(r'^open\s([亜-熙ぁ-んァ-ヶa-zA-Z._?\/^\S].*?)\/([亜-熙ぁ-んァ-ヶa-zA-Z._?\/^\S].*?)'
@@ -44,4 +58,16 @@ def reply_test(message, title, overview, level, date):
                               'のタスクが存在するよ？おっちょこちょいのリトルデーモンだね :grin: \n'
             message.reply(default_message)
 
-            connection.close()
+
+@respond_to(r'^close\s([亜-熙ぁ-んァ-ヶa-zA-Z._?\/^\S].*?)$')
+@listen_to(r'^close\s([亜-熙ぁ-んァ-ヶa-zA-Z._?\/^\S].*?)$')
+def close_task(message, title):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM Task WHERE username = 'yudai' AND title ='" + title + "'")
+        data = cursor.fetchone()
+        if data is None:
+            message.reply("そんなタスクないよ!!")
+        else:
+            cursor.execute("UPDATE Task SET status ='off' WHERE username ='yudai' AND title ='" + title + "'")
+            connection.commit()
+            message.reply("タスクを閉じたよ!!")
